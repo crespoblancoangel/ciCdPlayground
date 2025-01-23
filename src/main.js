95eab2f1668945db4545a90034d3df0a0f90b8a6
crespoblancoangel <crespoblancoangel@gmail.com>
2025-01-23 17:43:19 +0100

    Personalize main.js

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
+    name: "Ángel Crespo Blanco",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: ["Production"],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+    tools: "None yet",
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    expectations: ["Get some basic experience about CI/CD"],
   },
 });
